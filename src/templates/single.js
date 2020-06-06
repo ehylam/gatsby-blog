98405facc68bc6ea3ed770fdc8e6e88d83d5038f
Eric Lam <ehylam.p@gmail.com>
2020-06-06 14:59:34 +1000

    TEST

--- src/templates/single.js
+++ src/templates/single.js
@@ -10,32 +10,32 @@ import Layout from "../components/layout"
 
 const SinglePage = ({data}) => {
 
-  const {markdownRemark} = data
-  const {frontmatter,html} = markdownRemark
+  // const {markdownRemark} = data
+  // const {frontmatter,html} = markdownRemark
 
-  return (
-    <Layout>
-    <SEO title="Post Title Here"/>
-      <section className="single">
-      <h1>{frontmatter.title}</h1>
-      <div className="content" dangerouslySetInnerHTML={{__html: frontmatter.postcontent }}/>
-      </section>
-    </Layout>
-   );
+  // return (
+  //   <Layout>
+  //   <SEO title="Post Title Here"/>
+  //     <section className="single">
+  //     <h1>{frontmatter.title}</h1>
+  //     <div className="content" dangerouslySetInnerHTML={{__html: frontmatter.postcontent }}/>
+  //     </section>
+  //   </Layout>
+  //  );
 }
 
 export default SinglePage;
 
 
 
-export const singleQuery = graphql`
-  query singlePageQuery {
-    markdownRemark {
-      frontmatter {
-        title
-        postcontent
-      }
-      id
-    }
-  }
-`
+// export const singleQuery = graphql`
+//   query singlePageQuery {
+//     markdownRemark {
+//       frontmatter {
+//         title
+//         postcontent
+//       }
+//       id
+//     }
+//   }
+// `
