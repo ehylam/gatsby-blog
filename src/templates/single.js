7a53c646dc20f2ef10fe61f9c2df1c5e298b969b
Eric Lam <ehylam.p@gmail.com>
2020-06-06 13:47:53 +1000

    removed unused variables

--- src/templates/single.js
+++ src/templates/single.js
@@ -10,15 +10,15 @@ import Layout from "../components/layout"
 
 const SinglePage = ({data}) => {
 
-  const {markdownRemark} = data
-  const {frontmatter,html} = markdownRemark
+  // const {markdownRemark} = data
+  // const {frontmatter,html} = markdownRemark
 
   return (
     <Layout>
     <SEO title="Post Title Here"/>
       <section className="single">
-      <h1>{frontmatter.title}</h1>
-      <div className="content" dangerouslySetInnerHTML={{__html: frontmatter.info }}/>
+      {/* <h1>{frontmatter.title}</h1>
+      <div className="content" dangerouslySetInnerHTML={{__html: frontmatter.info }}/> */}
       </section>
     </Layout>
    );
